22def2e2fd3b3e7c05f9f16bc5ed9613d9dfc62e
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2015-07-15 23:15:46 +0000

    updated project frontend dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,10 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
+    mix.sass('admin.scss');
     mix.sass('app.scss');
+    mix.scripts([
+    	'../../assets/bower/jquery/dist/jquery.js',
+    	'../../assets/bower/bootstrap-sass-official/assets/javascripts/bootstrap.js'
+    ], 'public/js/vendor.js');
 });
